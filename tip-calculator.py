30a8a175ee86afc000061aaaa57dcaf55bd25061
nguyenscg <nguyencsg@gmail.com>
2024-09-09 12:56:19 -0500

    converted bill to be a float rather than a string. added dollar sign

--- tip-calculator.py
+++ tip-calculator.py
@@ -1,8 +1,8 @@
 # This should print as the first message
 print("Welcome to the Tip Calculator!")
 # first question prompt
-totalbill = input("What was the total bill?")
+bill = float(input("What was the total bill? $"))
 tip = input("How much tip would you like to give? 10, 12, or 15?")
 people = input("How many people to split the bill?")
-finalTotal = (totalbill * tip / people)
+finalTotal = (bill * tip / people)
 print("Each person should pay: " + str(finalTotal))
