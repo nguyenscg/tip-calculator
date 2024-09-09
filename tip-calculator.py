f3d2bf96a62d7ec73c071f37c2f22d51ad8e645b
nguyenscg <nguyencsg@gmail.com>
2024-09-09 12:58:32 -0500

    converted input strings to be integers

--- tip-calculator.py
+++ tip-calculator.py
@@ -2,7 +2,7 @@
 print("Welcome to the Tip Calculator!")
 # first question prompt
 bill = float(input("What was the total bill? $"))
-tip = input("How much tip would you like to give? 10, 12, or 15?")
-people = input("How many people to split the bill?")
+tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
+people = int(input("How many people to split the bill?"))
 finalTotal = (bill * tip / people)
 print("Each person should pay: " + str(finalTotal))
