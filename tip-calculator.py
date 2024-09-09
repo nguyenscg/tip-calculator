3959fd2f21d9eb83b3f032d4d1f764e8eee0cf17
nguyenscg <nguyencsg@gmail.com>
2024-09-09 13:05:48 -0500

    used round function to round to 2 decimal places. use f strings to print the final total

--- tip-calculator.py
+++ tip-calculator.py
@@ -4,5 +4,7 @@ print("Welcome to the Tip Calculator!")
 bill = float(input("What was the total bill? $"))
 tip = int(input("How much tip would you like to give? 10, 12, or 15?"))
 people = int(input("How many people to split the bill?"))
-finalTotal = (bill * tip / people)
-print("Each person should pay: " + str(finalTotal))
+total_bill = tip / 100 * bill + bill
+bill_per_person = total_bill / people
+final_total = round(bill_per_person, 2) #This will round to 2 decimal places
+print(f"Each person should pay ${final_total}")
